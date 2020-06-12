bd5dad6b26e1d1694908c8c4d1d7f4b1528d775a
amanbatra08 <48562256+amanbatra08@users.noreply.github.com>
2020-06-12 18:32:50 +0530

    aman

---
+++ fastecom/fastecom/app/src/main/java/com/edevelopers/fastecom/MessageActivity.java
@@ -0,0 +1,48 @@
+package com.edevelopers.fastecom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+public class MessageActivity extends AppCompatActivity {
+
+    TextView username;
+    ImageView profile_image;
+    ImageButton btn_send;
+    EditText text_send;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        profile_image = findViewById(R.id.profile_image);
+        username = findViewById(R.id.username);
+        btn_send = findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
+
+        intent = getIntent();
+        String userId = intent.getStringExtra("userId");
+
+    }
+}
